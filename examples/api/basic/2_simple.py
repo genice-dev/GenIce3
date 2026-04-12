7578c4b9dc3907d96433e6a45f8940d2cd271008
vitroid <vitroid@gmail.com>
2026-04-12 23:37:36 +0900

    Change exporter from gromacs to yaplot in example script

--- examples/api/basic/2_simple.py
+++ examples/api/basic/2_simple.py
@@ -2,7 +2,7 @@ from genice3.genice import GenIce3
 from genice3.plugin import Exporter
 
 # corresponding command:
-# genice3 A15 --exporter gromacs
+# genice3 A15 --exporter yaplot
 genice = GenIce3()
 genice.set_unitcell("A15")
-Exporter("gromacs").dump(genice)
+Exporter("yaplot").dump(genice)
